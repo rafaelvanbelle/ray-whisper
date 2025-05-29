a79dcae9f59243f90ef97135bcb28ef78ba5771c
Rafael Van Belle <rafael@dataroots.io>
2025-05-29 17:38:31 +0200

    update

--- ui/gradio_ui_progress.py
+++ ui/gradio_ui_progress.py
@@ -200,6 +200,5 @@ if __name__ == "__main__":
     iface.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        show_error=True,
-        enable_queue=True
+        show_error=True
     )
